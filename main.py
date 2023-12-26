3637aa99dc8180ba2ad41c365cd6da943a263498
Svtrnx <sasha.boyku@mail.ru>
2023-12-26 21:17:11 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:1420"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
